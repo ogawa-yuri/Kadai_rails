1b91d7ccbdbfe382a081f1d2f1d6b0da4a12279b
ogawa-yuri <nomiyuri713@gmail.com>
2021-06-06 18:00:16 +0900

    ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to pictures_path
+    else
+      render :new
+    end
+  end
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_042706) do
+ActiveRecord::Schema.define(version: 2021_06_06_064542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2021_06_05_042706) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
